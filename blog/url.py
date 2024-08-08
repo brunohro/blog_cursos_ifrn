0721d72cf3b41fa2657da79d41490a14771cd115
Bruno Henrique Oliveira <bh084996074995@gmail.com>
2024-08-08 10:38:22 -0300

    Criando as views, urls e templates

--- blog/url.py
+++ blog/url.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import *
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('ads/', ads, name='ads'),
+    path('agro_industria/', agro_industria, name='agro_industria'),
+    path('alimentos/', alimentos, name='alimentos'),
+    path('apicultura/', apicultura, name='apicultura'),
+    path('informatica/', informatica, name='informatica'),
+    path('quimica/', quimica, name='quimica'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'blog/index.html')
+
+def ads(request):
+    return render(request, 'blog/ads.html')
+
+def agro_industria(request):
+    return render(request, 'blog/agro_industria.html')
+
+def alimentos(request):
+    return render(request, 'blog/alimentos.html')
+
+def apicultura(request):
+    return render(request, 'blog/apicultura.html')
+
+def informatica(request):
+    return render(request, 'blog/informatica.html')
+
+def quimica(request):
+    return render(request, 'blog/quimica.html')
